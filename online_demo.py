5a00a5043e7dbc76329a9ca6e8f5af72cd43666d
apirrone <awanegenebayo@gmail.com>
2024-03-05 14:44:56 +0100

    adding deps in setup.py. click on image to set points in online_demo.py. adding .gitignore

--- online_demo.py
+++ online_demo.py
@@ -4,14 +4,18 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
-import torch
 import argparse
-import imageio.v3 as iio
-import numpy as np
+import os
+import time
 
-from cotracker.utils.visualizer import Visualizer
+import cv2
+import imageio.v3 as iio
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
 from cotracker.predictor import CoTrackerOnlinePredictor
+from cotracker.utils.visualizer import Visualizer
 
 # Unfortunately MPS acceleration does not support all the features we require,
 # but we may be able to enable it in the future
@@ -22,6 +26,22 @@ DEFAULT_DEVICE = (
     if torch.cuda.is_available()
     else "cpu"
 )
+
+cv2.namedWindow("first_frame")
+
+points = []
+
+
+def on_mouse(event, x, y, flags, param):
+    global points
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points.append((x, y))
+        print(x, y)
+
+
+# callback function
+cv2.setMouseCallback("first_frame", on_mouse)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -45,6 +65,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    cap = cv2.VideoCapture(args.video_path)
+    first_frame = cap.read()[1]
+    while True:
+        im = first_frame.copy()
+        for point in points:
+            im = cv2.circle(im, point, 3, (0, 0, 255), -1)
+        cv2.imshow("first_frame", im)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
     if not os.path.isfile(args.video_path):
         raise ValueError("Video file does not exist")
 
@@ -56,9 +87,13 @@ if __name__ == "__main__":
 
     window_frames = []
 
-    def _process_step(window_frames, is_first_step, grid_size, grid_query_frame):
+    def _process_step(
+        window_frames, is_first_step, grid_size, grid_query_frame, queries
+    ):
         video_chunk = (
-            torch.tensor(np.stack(window_frames[-model.step * 2 :]), device=DEFAULT_DEVICE)
+            torch.tensor(
+                np.stack(window_frames[-model.step * 2 :]), device=DEFAULT_DEVICE
+            )
             .float()
             .permute(0, 3, 1, 2)[None]
         )  # (1, T, 3, H, W)
@@ -66,38 +101,58 @@ if __name__ == "__main__":
             video_chunk,
             is_first_step=is_first_step,
             grid_size=grid_size,
+            queries=torch.Tensor(queries).to(DEFAULT_DEVICE),
             grid_query_frame=grid_query_frame,
         )
 
     # Iterating over video frames, processing one window at a time:
     is_first_step = True
+    times = []
+    start = time.time()
     for i, frame in enumerate(
         iio.imiter(
             args.video_path,
             plugin="FFMPEG",
         )
     ):
+        s = time.time()
         if i % model.step == 0 and i != 0:
+            queries = np.zeros((1, len(points), 3))
+            for point in points:
+                queries[0, points.index(point), :] = (i, point[0], point[1])
             pred_tracks, pred_visibility = _process_step(
                 window_frames,
                 is_first_step,
-                grid_size=args.grid_size,
+                grid_size=0,
+                queries=queries,
                 grid_query_frame=args.grid_query_frame,
             )
             is_first_step = False
+        times.append(time.time() - s)
         window_frames.append(frame)
     # Processing the final video frames in case video length is not a multiple of model.step
+    queries = np.zeros((1, 1, 3))
+    queries[0, 0, :] = (i, 100, 100)
     pred_tracks, pred_visibility = _process_step(
         window_frames[-(i % model.step) - model.step - 1 :],
         is_first_step,
-        grid_size=args.grid_size,
+        grid_size=0,
+        queries=queries,
         grid_query_frame=args.grid_query_frame,
     )
-
+    print("Time elapsed: ", time.time() - start, " seconds")
+    fps = len(times) / (time.time() - start)
+    print("FPS: ", fps)
+    # plt.plot(np.arange(len(times)), times)
+    # plt.show()
     print("Tracks are computed")
 
     # save a video with predicted tracks
     seq_name = args.video_path.split("/")[-1]
-    video = torch.tensor(np.stack(window_frames), device=DEFAULT_DEVICE).permute(0, 3, 1, 2)[None]
-    vis = Visualizer(save_dir="./saved_videos", pad_value=120, linewidth=3)
-    vis.visualize(video, pred_tracks, pred_visibility, query_frame=args.grid_query_frame)
+    video = torch.tensor(np.stack(window_frames), device=DEFAULT_DEVICE).permute(
+        0, 3, 1, 2
+    )[None]
+    vis = Visualizer(save_dir="./saved_videos", pad_value=120, linewidth=3, fps=15)
+    vis.visualize(
+        video, pred_tracks, pred_visibility, query_frame=args.grid_query_frame
+    )
--- setup.py
+++ setup.py
@@ -9,7 +9,14 @@ from setuptools import find_packages, setup
 setup(
     name="cotracker",
     version="2.0",
-    install_requires=[],
+    install_requires=[
+        "torch==2.2.1",
+        "torchvision==0.17.1",
+        "imageio==2.34.0",
+        "imageio-ffmpeg==0.4.9",
+        "matplotlib==3.8.3",
+        "opencv-python==4.9.0.80",
+    ],
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["matplotlib"],
